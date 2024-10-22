1fd531e7737188c2335839a965761e1d3de882df
Adam Katz <adam.katz97@gmail.com>
2024-10-22 12:57:44 -0500

    Update streamilt_app.py
    changed session connection

--- streamilt_app.py
+++ streamilt_app.py
@@ -11,13 +11,14 @@ st.write(
 
 
 
-
+cnx=st.connection("snowflake")
+session=cnx.session()
 name_on_order = st.text_input('Name on Smoothie')
 st.write("The name on your Smoothie will be:", name_on_order)
 
 
 
-session = get_active_session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 
